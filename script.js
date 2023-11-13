f139f53c2c89debc9c0a99e1eddbda7383d50219
alfozan abdulaziz <98517079+AMF21@users.noreply.github.com>
2023-11-13 07:58:28 +0000

    Update script.js

--- script.js
+++ script.js
@@ -10,6 +10,17 @@ getReposButton.addEventListener('click', async () => {
     const url = `https://api.github.com/users/${username}/repos`;
     const ulElement = document.getElementById('repos-list');
     // Fetch and display the list of repositories as li elements inside the ul element.
+    const response = await fetch(url);
+    const data = await response.json;
 
+      data.forEach((repo) => {
+                const listItem = document.createElement('li');
+             const link = document.createElement('a');
+                link.href=repo.html_url
+                link.textContent=repo.name 
+                listItem.innerHTML = repo.full_name;
+                ulElement.appendChild(listItem);
+            });
+    
 });
 
